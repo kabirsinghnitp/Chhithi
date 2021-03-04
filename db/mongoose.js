801888546529d9a8bd7fa79fa0141c1126f52d15
kabirsinghnitp <76893101+kabirsinghnitp@users.noreply.github.com>
2021-03-04 16:22:06 +0530

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const mongodb_uri = 'mongodb+srv://voidrohit:Rks&18158920@cluster0.oiqtt.mongodb.net/ChatApp?retryWrites=true&w=majority'
-const local_mongodb_uri = 'mongodb://127.0.0.1:27017/ChatApp'
+const mongodb_uri = databaseLink
+const local_mongodb_uri = 'mongodb://127.0.0.1:27017/Chhithi'
 mongoose.connect(mongodb_uri, {
     useNewUrlParser:true,
     useCreateIndex: true,
